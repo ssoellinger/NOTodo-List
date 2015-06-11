5bab13263200b1c2a64f1fdf25c61415f6fe09bf
droithmayr <dani.roithi@yahoo.com>
2015-06-11 13:16:16 +0200

    Create layouts and DetailFragment

---
+++ app/src/main/java/projekt/htlgrieskirchen/at/notodoslist/DetailFragment.java
@@ -0,0 +1,13 @@
+package projekt.htlgrieskirchen.at.notodoslist;
+
+
+import android.app.Fragment;
+
+import java.util.Arrays;
+
+/**
+ * Created by droithmayr on 11.06.2015.
+ */
+public class DetailFragment extends Fragment{
+
+}
