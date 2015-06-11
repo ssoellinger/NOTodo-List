48c071b69b018b468afc74a86a96d58db889969e
droithmayr <dani.roithi@yahoo.com>
2015-06-11 12:24:10 +0200

    Add MyTodoOpenHelper

--- app/src/main/java/projekt/htlgrieskirchen/at/notodoslist/MyTodoOpenHelper.java
+++ app/src/main/java/projekt/htlgrieskirchen/at/notodoslist/MyTodoOpenHelper.java
@@ -1,7 +1,29 @@
 package projekt.htlgrieskirchen.at.notodoslist;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by droithmayr on 11.06.2015.
  */
-public class MyTodoOpenHelper {
+public class MyTodoOpenHelper extends SQLiteOpenHelper
+{
+    private final static String DB_NAME = "Todos.db";
+    private final static int DB_VERSRION = 1;
+
+    public MyTodoOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSRION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL((TodosTbl.SQL_CREATE));
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(TodosTbl.SQL_DROP);
+        onCreate(db);
+    }
 }
